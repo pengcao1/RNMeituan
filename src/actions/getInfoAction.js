3a7340ca64c892228864c519294696cd080f012f
Peng.Cao <pcao@thoughtworks.com>
2018-08-28 07:00:05 +0800

    re write action to arr function

--- src/actions/getInfoAction.js
+++ src/actions/getInfoAction.js
@@ -1,17 +1,15 @@
 import { FETCH_GETS } from "./types";
 import * as API from "../../src/API/api"
 
-export function fetchInfos() {
-    return function(dispatch) {
-        fetch(API.GET_DISCOUNT)
-            .then(res => {
-                return res.json();
+export const fetchInfos = () => dispatch => {
+    fetch(API.GET_DISCOUNT)
+        .then(res => {
+            return res.json();
+        })
+        .then(info => {
+            dispatch({
+                type: FETCH_GETS,
+                payload: info.data
             })
-            .then(info => {
-                dispatch({
-                    type: FETCH_GETS,
-                    payload: info.data
-                })
-            })
-    }
+        })
 }
